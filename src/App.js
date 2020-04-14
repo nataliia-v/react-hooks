5817e4d7bbf19e2d3e9943cb174f4de2ddd2ec2b
nataliia-v <nataly.verbenskaya@gmail.com>
2020-04-14 23:09:13 +0300

    + increment count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(prevState => prevState + 1);
+    };
+
   return (
     <div className="App">
-      hello
+      <button onClick={handleClick}>I was clicked {count} times</button>
     </div>
   );
 }
