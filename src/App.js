4ba4c9c4003e6115535c355c778cb65afe647836
nataliia-v <nataly.verbenskaya@gmail.com>
2020-04-14 23:27:49 +0300

    + toggle light (img)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, { useState } from 'react';
 
 function App() {
     const [count, setCount] = useState(0);
+    const [isOn, setIsOn] = useState(false);
 
     const handleClick = () => {
         setCount(prevState => prevState + 1);
     };
 
+    const handleChangeLight = () => {
+      setIsOn(prevState => !prevState)
+    };
+
   return (
     <div className="App">
+        <h2>Counter</h2>
       <button onClick={handleClick}>I was clicked {count} times</button>
+
+        <h2>Toggle light</h2>
+        <img
+            style={{
+            height: "50px",
+            width: "50px",
+        }}
+            onClick={handleChangeLight}
+            src={
+                isOn
+                    ? "https://icon.now.sh/highlight/fd0"
+                    : "https://icon.now.sh/highlight/aaa"
+            }
+        />
     </div>
   );
 }
