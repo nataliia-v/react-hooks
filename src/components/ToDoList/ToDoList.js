feb8ba7d3831bfe1a5c40e26988c0a60b42760fb
nataliia-v <nataly.verbenskaya@gmail.com>
2020-04-15 22:28:05 +0300

    + toggling todos

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,10 +1,11 @@
 import React, { useContext, useReducer } from 'react';
 
-import {TodosContext} from "../../context";
+import { TodosContext } from "../../context";
+import { TOGGLE_TODO } from '../../constsnts';
 
 
 export default function ToDoList () {
-    const { state } = useContext(TodosContext);
+    const { state, dispatch } = useContext(TodosContext);
 
     const title = state.todos.length > 0 ? `${state.todos.length} Todos` : "Nothing To Do!";
     return (
@@ -13,7 +14,7 @@ export default function ToDoList () {
             <ul>
                 {state.todos.map(todo => (
                     <li className="bg-orange-dark flex items-center border-black border-dashed border-2 my-2 py-4" key={todo.id}>
-                        <span className="flex-1 ml-12">{todo.text}</span>
+                        <span onDoubleClick={()=> dispatch({type: TOGGLE_TODO, payload: todo})} className={`flex-1 ml-12 ${todo.complete && "line-through text-grey-darkest"}`}>{todo.text}</span>
                         <button>
                             <img src="https://icon.now.sh/edit/0050c5" alt="Edit icon" className="h-6"/>
                         </button>
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,19 @@
+import { TOGGLE_TODO } from './constsnts';
+
 export const todosReducer = (state, action) => {
 
     switch (action.type) {
 
+        case TOGGLE_TODO:
+            const toggledTodos = state.todos.map(
+                todo => todo.id === action.payload.id
+                    ? { ...action.payload, complete: !action.payload.complete }
+                    : todo
+            );
+            return {
+               ...state,
+               todos: toggledTodos
+            };
         default:
             return state
     }
