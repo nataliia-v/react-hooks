2dd760b6d62347d5362dd48c1a0abf1e617ccf20
nataliia-v <nataly.verbenskaya@gmail.com>
2020-04-15 17:37:31 +0300

    + fetching data upon submitting form

--- src/components/requests/AxiosRequests.js
+++ src/components/requests/AxiosRequests.js
@@ -11,16 +11,24 @@ export default function AxiosRequests () {
 
     useEffect( () => {
         getResults();
-    }, [query]);
+    }, []);
 
     const getResults = async () => {
         const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
         setResults(response.data.hits)
     };
+    const handleSearch = (event)=> {
+        event.preventDefault();
+        getResults();
+    };
 
     return (
         <>
-            <input type="text" onChange={(event) => setQuery(event.target.value)}/>
+            <form onSubmit={handleSearch}>
+                <input type="text" onChange={(event) => setQuery(event.target.value)} value={query}/>
+                <button type="submit">search</button>
+            </form>
+
             <ul>
                 {results.map(result => (
                     <li key={result.objectID}>
