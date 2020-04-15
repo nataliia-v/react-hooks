b98c8a2873dadae6b126a5a816cf4c28fbe46de7
nataliia-v <nataly.verbenskaya@gmail.com>
2020-04-15 21:00:32 +0300

    + replacing Redux with the useReducer Hook on Counter example

--- src/components/completeCRUDApp/App.js
+++ src/components/completeCRUDApp/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useReducer } from 'react';
 
 import Navbar from "./Navbar";
 
@@ -6,13 +6,47 @@ import { UserContext } from '../../index'
 
 import './index.css';
 
+export const initialState = {
+    count: 0,
+};
+
+export const reducer = (state, action) => {
+
+    switch (action.type) {
+        case 'increment':
+            return {
+                ...state,
+                count: state.count + 1,
+            };
+        case 'decrement':
+            return {
+                ...state,
+                count: state.count - 1,
+            };
+        case 'reset':
+            return initialState;
+        default:
+            return initialState
+    }
+};
+
 export default function Application () {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
 
     const value = useContext(UserContext);
 
     return (
         <div>
             <h2>Hello, {value.username}</h2>
+            <div>
+                Count: {state.count}
+                <button className="border p-1" onClick={()=> dispatch({type: 'increment'})}>increment</button>
+                <button className="border p-1" onClick={()=> dispatch({type: 'decrement'})}>decrement</button>
+                <button className="border p-1" onClick={()=> dispatch({type: 'reset'})}>reset</button>
+            </div>
+
+
             <Navbar/>
         </div>
     )
