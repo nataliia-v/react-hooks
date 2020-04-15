b0f3f7fa8233e393bf7e5c52c82e1c394ba6e951
nataliia-v <nataly.verbenskaya@gmail.com>
2020-04-15 16:33:40 +0300

    + getResults async fnc

--- src/components/requests/AxiosRequests.js
+++ src/components/requests/AxiosRequests.js
@@ -6,15 +6,16 @@ import axios from 'axios';
 
 export default function AxiosRequests () {
 
-    const [results, setResults] = useState([])
+    const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-            .then(response => {
-                console.log(response.data);
-                setResults(response.data.hits)
-            })
+    useEffect( () => {
+        getResults();
     }, []);
+
+    const getResults = async () => {
+        const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks');
+        setResults(response.data.hits)
+    };
 
     return (
         <>
